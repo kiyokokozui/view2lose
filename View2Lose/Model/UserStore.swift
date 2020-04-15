4c5c62022ba58eee216bbf6a2dfb4a04fb39071e
Sagar Chhetri <schhet07@gmail.com>
2020-04-15 16:36:51 +1000

    first commit

---
+++ View2Lose/Model/UserStore.swift
@@ -0,0 +1,21 @@
+//
+//  UserStore.swift
+//  View2Lose
+//
+//  Created by Sagar on 12/4/20.
+//  Copyright © 2020 Sagar. All rights reserved.
+//
+
+import SwiftUI
+
+struct UserStore: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserStore_Previews: PreviewProvider {
+    static var previews: some View {
+        UserStore()
+    }
+}
